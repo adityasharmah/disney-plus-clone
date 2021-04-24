18449df6e48beb57706df593abf1f1485a850d67
adityasharma2oo2 <adityasharma9319635@gmail.com>
2021-04-25 04:16:14 +0530

    creating home page and image slider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './components/Login';
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         <Switch>
           <Route exact path = '/'>
             <Login/>
+          </Route>
+          <Route  path ='/home'>
+            <Home />
           </Route>
         </Switch>
       </Router>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
+import {useEffect} from 'react';
 import styled from 'styled-components';
 import {useDispatch,useSelector } from 'react-redux';
 import {useHistory} from 'react-router-dom';
 import {auth,provider} from '../firebase';
-import {selectUserName,selectUserPhoto,setUserLoginDetails} from '../features/user/userSlice'
+import {
+  selectUserName,
+  selectUserPhoto,
+  setUserLoginDetails,
+  setSignOutState,
+} from '../features/user/userSlice'
 
 
 
@@ -13,21 +19,41 @@ const Header = (props) =>{
   const userName = useSelector(selectUserName);
   const userPhoto = useSelector(selectUserPhoto);
 
+  useEffect(()=>{
+    auth.onAuthStateChanged(async(user) =>{
+      if(user){
+        setUser(user)
+        history.push('/home')
+      }
+    })
+  },[userName])
+
 
   const handleAuth = () => {
-    auth.signInWithPopup(provider).then((result) => {
+    if(!userName){
+
+    
+    auth
+     .signInWithPopup(provider)
+     .then((result) => {
       setUser(result.user);
-    }).catch((error) =>{
+    })
+    .catch((error) =>{
       alert(error.message);
     });
-  };
-
+  }else if (userName){
+    auth.signOut().then(() =>{
+      dispatch(setSignOutState())
+      history.push("/");
+    }).catch((err) =>alert(err.message));
+  }
+};
   const setUser = (user) =>{
     dispatch(
       setUserLoginDetails({
         name:user.displayName,
         email:user.email,
-        photo:user.photURL,
+        photo:user.photoURL,
       
       })
     );
@@ -76,7 +102,12 @@ const Header = (props) =>{
         </a>
 
       </NavMenu>
-      <UseImg  src = {userPhoto} alt ={userName} />
+      <SignOut>
+         <UserImg  src = {userPhoto} alt ={userName} />
+         <DropDown>
+           <span onClick = {handleAuth}>Sign Out</span>
+         </DropDown>
+      </SignOut>
       </>
     )}
     </Nav>
@@ -202,9 +233,49 @@ transition:all .2s ease 0s;
 }
 `;
 
-const UseImg = styled.img`
+const UserImg = styled.img`
 height:100%;
 `;
+
+const DropDown = styled.div`
+position:absolute;
+top:48px;
+right:0px;
+background :rgb(19,19,19);
+border:1px solid rgba(151,151,151,0.34);
+border-radius:4px;
+box-shadow:rgb(0,0,0/50%) 0px 0px 18px 0px;
+padding:10px;
+font-size:14px;
+letter-spacing:3px;
+width:100px;
+opacity:0;
+
+`;
+
+const SignOut = styled.div`
+position:realtive;
+height:48px;
+width:48px;
+display:flex;
+cursor:pointer;
+align-items:center;
+justify-content:center;
+
+${UserImg}{
+  border-radius:50%;
+  width:100%;
+  height:100%;
+}
+
+&:hover{
+  ${DropDown}{
+    opacity:1;
+    transition-duration: 1s;
+  }
+}
+`;
+
 
 
 
